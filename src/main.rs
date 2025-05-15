360c7098fc2d69a75a2b277f98605e58c6fb9ac5
Øyvind Ege <oyvind.ege@pm.me>
2025-05-15 13:22:11 +0200

    moved render logic to trait impl

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@ struct Board {
     rows: Vec<Vec<u8>>,
 }
 
+trait Render {
+    fn render(&self) {}
+}
+
 impl Board {
     pub fn new() -> Board {
+        // 32 = player X
+        // 64 = player Y
         Board {
             rows: vec![vec![32, 0, 32], vec![0, 32, 0], vec![0, 0, 32]],
         }
     }
+}
 
-    pub fn render(&self) {
+impl Render for Board {
+    fn render(&self) {
         for row in &self.rows {
             for (i, cell) in row.iter().enumerate() {
                 match cell {
                     32 => print!("X"),
+                    64 => print!("Y"),
                     _ => print!(" "),
                 }
                 if i < 2 {
@@ -29,6 +38,8 @@ impl Board {
 
 fn main() {
     println!("Welcome to tic tac toe.");
+
     let b = Board::new();
+
     b.render();
 }
