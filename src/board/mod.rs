84c931b9629c452f9c4d280ee47850e00895b43a
Øyvind Ege <oyvind.ege@pm.me>
2025-05-25 15:00:34 +0200

    Redid Board and started working on Render
    Board now consists of a single u8 vector for all data. Still working on
    render function.

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,9 +1,113 @@
+use crate::Game;
+
 pub struct Board {
     data: Vec<u8>,
     width: usize,
 }
 
+const BOARD_STANDARD_SIZE: usize = 3;
+
 impl Board {
+    pub fn new() -> Board {
+        Board {
+            data: vec![0; BOARD_STANDARD_SIZE * BOARD_STANDARD_SIZE],
+            width: BOARD_STANDARD_SIZE,
+        }
+    }
+
+    // NOTE: We currently do not check if there is already a piece here. May need to return an Option, or a Result
+    pub fn place(&mut self, index: usize, piece_to_place: u8) {
+        self.data[index] = piece_to_place;
+    }
+
+    pub fn render(&self, game: &Game) {
+        println!();
+        println!("The board currently looks like this:");
+        for (i, cell) in self.data.iter().enumerate() {
+            match *cell {
+                val if val == game.player_1.encoded => print!("{}", game.player_1.name),
+                val if val == game.player_2.encoded => print!("{}", game.player_2.name),
+                val => print!("{val}"),
+            }
+            if i < 2 || i > 2 && i < 5 || i > 5 && i < 8 {
+                print!(" | ");
+            }
+
+            if i == 2 || i == 5 || i == 8 {
+                println!();
+            }
+        }
+        println!("__|___|___");
+        println!();
+    }
+
+    pub fn render_help(&self) {
+        println!();
+        println!("This is how you designate the board cells:");
+        for (index, _) in self.data.iter().enumerate() {
+            print!("{}", index);
+            if index < 2 {
+                print!(" | ");
+            }
+        }
+        println!();
+        println!("__|___|___");
+        println!();
+    }
+
+    /// Return the encoded player symbol if they have won, None if no victor
+    pub fn check_for_victory(&self) -> Option<u8> {
+        self.has_horizontal_victor()
+            .or_else(|| self.has_vertical_victor())
+            .or_else(|| self.has_diagonal_victor())
+    }
+
+    fn has_horizontal_victor(&self) -> Option<u8> {
+        for offset in 0..self.width {
+            self.has_victor(&self.data[offset..offset + self.width]);
+        }
+        None
+    }
+
+    fn has_vertical_victor(&self) -> Option<u8> {
+        //TODO: Rewrite. Can be much simpler I am sure.
+        let mut verticals: Vec<Vec<u8>> = vec![vec![], vec![], vec![]];
+
+        for i in 0..self.width {
+            verticals.push(self.get_column(i));
+        }
+
+        for v in verticals {
+            let has_victor = self.has_victor(&v);
+            if has_victor.is_some() {
+                return has_victor;
+            }
+        }
+
+        None
+    }
+
+    fn has_diagonal_victor(&self) -> Option<u8> {
+        let diagonal_1 = self.get_diagonal(1);
+        let diagonal_2 = self.get_diagonal(2);
+
+        self.has_victor(&diagonal_1)
+            .or_else(|| self.has_victor(&diagonal_2))
+    }
+
+    fn has_victor(&self, vec: &[u8]) -> Option<u8> {
+        let first = vec.first();
+        first?;
+        if *first.unwrap() == 0 {
+            return None;
+        }
+        if vec.iter().all(|&x| x == *first.unwrap()) {
+            Some(*first.unwrap())
+        } else {
+            None
+        }
+    }
+
     pub fn get_adjacents(&self, cell_number: usize) -> Vec<u8> {
         self.data
             .iter()
@@ -41,7 +145,18 @@ impl Board {
     }
 
     pub fn get_diagonal(&self, diagonal_num: usize) -> Vec<u8> {
-        todo!();
+        if diagonal_num > 1 {
+            return vec![];
+        }
+        let size_coefficient = self.width - 1;
+        let step = 4 / std::cmp::max(1, size_coefficient * diagonal_num);
+        self.data
+            .iter()
+            .skip(diagonal_num * size_coefficient)
+            .step_by(step)
+            .take(self.width)
+            .copied()
+            .collect()
     }
 }
 
@@ -165,7 +280,7 @@ mod tests {
             let data = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
             let b = Board { data, width: 3 };
 
-            assert_eq!(b.get_diagonal(0), vec![0, 3, 6]);
+            assert_eq!(b.get_diagonal(0), vec![0, 4, 8]);
         }
 
         #[test]
@@ -173,15 +288,7 @@ mod tests {
             let data = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
             let b = Board { data, width: 3 };
 
-            assert_eq!(b.get_diagonal(1), vec![1, 4, 7]);
-        }
-
-        #[test]
-        fn third() {
-            let data = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
-            let b = Board { data, width: 3 };
-
-            assert_eq!(b.get_diagonal(2), vec![2, 5, 8]);
+            assert_eq!(b.get_diagonal(1), vec![2, 4, 6]);
         }
 
         #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod board;
-/*
-struct Game<'a> {
+use crate::board::Board;
+use std::error;
+use std::io;
 
+struct Game<'a> {
     board: Board,
     input_controller: InputControl,
     exit_wanted: bool,
@@ -14,13 +16,8 @@ struct Player<'a> {
     encoded: u8,
 }
 
-struct Coordinate {
-    x: usize,
-    y: usize,
-}
-
 enum InputType {
-    Coord(Coordinate),
+    Coord(usize),
     Exit,
     Help,
 }
@@ -90,189 +87,18 @@ impl InputControl {
         match input {
             val if val == "help" || val == "Help" || val == "HELP" => Some(InputType::Help),
             val if val == "exit" || val == "Exit" || val == "EXIT" => Some(InputType::Exit),
-            val if val.parse::<u8>().is_ok() => match self.get_coords_from_input(val) {
-                Ok(coord) => Some(InputType::Coord(coord)),
-                Err(_) => None,
-            },
+            val if val.parse::<usize>().is_ok() => {
+                Some(InputType::Coord(val.parse::<usize>().unwrap()))
+            }
+
             _ => None,
         }
     }
-
-    fn get_coords_from_input(&self, string: &str) -> Result<Coordinate, Box<dyn error::Error>> {
-        let num = string.parse::<u8>()?;
-        match num {
-            1 => Ok(Coordinate { x: 0, y: 0 }),
-            2 => Ok(Coordinate { x: 1, y: 0 }),
-            3 => Ok(Coordinate { x: 2, y: 0 }),
-            4 => Ok(Coordinate { x: 0, y: 1 }),
-            5 => Ok(Coordinate { x: 1, y: 1 }),
-            6 => Ok(Coordinate { x: 2, y: 1 }),
-            7 => Ok(Coordinate { x: 0, y: 2 }),
-            8 => Ok(Coordinate { x: 1, y: 2 }),
-            9 => Ok(Coordinate { x: 2, y: 2 }),
-            _ => Err("Invalid number. Try again.".into()),
-        }
-    }
 }
-
-impl Board {
-    pub fn new() -> Board {
-        Board {
-            rows: vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]],
-            number_of_columns: 3,
-        }
-    }
-
-    pub fn place(&mut self, coordinate: Coordinate, symbol_to_place: u8) {
-        self.rows[coordinate.y][coordinate.x] = symbol_to_place;
-    }
-
-    /// Return the encoded player symbol if they have won, None if no victor
-    pub fn check_for_victory(&self) -> Option<u8> {
-        self.has_horizontal_victor()
-            .or_else(|| self.has_vertical_victor())
-            .or_else(|| self.has_diagonal_victor())
-    }
-
-    fn has_horizontal_victor(&self) -> Option<u8> {
-        for row in &self.rows {
-            if let Some(victor) = self.has_victor(row) {
-                return Some(victor);
-            }
-        }
-        None
-    }
-
-    fn has_vertical_victor(&self) -> Option<u8> {
-        let mut verticals: Vec<Vec<u8>> = vec![vec![], vec![], vec![]];
-
-        for i in 0..self.number_of_columns {
-            verticals.push(self.get_vertical(i));
-        }
-
-        for v in verticals {
-            let has_victor = self.has_victor(&v);
-            if has_victor.is_some() {
-                return has_victor;
-            }
-        }
-
-        None
-    }
-
-    fn has_diagonal_victor(&self) -> Option<u8> {
-        let diagonal_1 = self.get_diagonal(1);
-        let diagonal_2 = self.get_diagonal(2);
-
-        self.has_victor(&diagonal_1)
-            .or_else(|| self.has_victor(&diagonal_2))
-    }
-
-    fn has_victor(&self, vec: &[u8]) -> Option<u8> {
-        let first = vec.first();
-        first?;
-        if *first.unwrap() == 0 {
-            return None;
-        }
-        if vec.iter().all(|&x| x == *first.unwrap()) {
-            Some(*first.unwrap())
-        } else {
-            None
-        }
-    }
-
-    /// Returns a new vector consisting of a "vertical" column in the board
-    /// * n represents the column number, from the right
-    fn get_vertical(&self, n: usize) -> Vec<u8> {
-        self.rows.iter().map(|row| row[n]).collect()
-    }
-
-    fn get_diagonal(&self, n: usize) -> Vec<u8> {
-        self.rows
-            .iter()
-            .enumerate()
-            .map(|(row_index, row)| {
-                let mut column_index = row_index;
-                if n == 2 {
-                    column_index = 2 - row_index;
-                }
-                row[column_index]
-            })
-            .collect()
-    }
-
-    fn render(&self, game: &Game) {
-        println!();
-        println!("The board currently looks like this:");
-        for row in &self.rows {
-            for (i, cell) in row.iter().enumerate() {
-                match *cell {
-                    val if val == game.player_1.encoded => print!("{}", game.player_1.name),
-                    val if val == game.player_2.encoded => print!("{}", game.player_2.name),
-                    _ => print!(" "),
-                }
-                if i < 2 {
-                    print!(" | ");
-                }
-            }
-            println!();
-            println!("__|___|___");
-        }
-        println!();
-    }
-
-    fn render_help(&self) {
-        println!();
-        println!("This is how you designate the board cells:");
-        for (row_index, row) in self.rows.iter().enumerate() {
-            for (col_index, _) in row.iter().enumerate() {
-                print!("{}", (row_index * self.number_of_columns + col_index) + 1);
-                if col_index < 2 {
-                    print!(" | ");
-                }
-            }
-            println!();
-            println!("__|___|___");
-        }
-        println!();
-    }
-}
-*/
 
 fn main() {
     println!("Welcome to tic tac toe.");
+    let game = Game::new();
+
+    game.board.render(&game);
 }
-
-/*
-#[cfg(test)]
-mod tests {
-    use super::Board;
-
-    #[test]
-    fn basic() {
-        let b = Board::new();
-        let row = vec![32, 32, 32];
-        assert_eq!(b.has_victor(&row), Some(32));
-    }
-
-    #[test]
-    fn basic_wrong() {
-        let b = Board::new();
-        let row = vec![0, 32, 32];
-        assert_eq!(b.has_victor(&row), None);
-    }
-
-    #[test]
-    fn whatever() {
-        let mut b = Board::new();
-        b.rows = vec![vec![32, 32, 0], vec![0, 0, 0], vec![64, 2, 0]];
-        assert_eq!(b.has_horizontal_victor(), None);
-    }
-
-    #[test]
-    fn whatever2() {
-        let mut b = Board::new();
-        b.rows = vec![vec![32, 32, 0], vec![64, 64, 64], vec![64, 2, 0]];
-        assert_eq!(b.has_horizontal_victor(), Some(64));
-    }
-} */
