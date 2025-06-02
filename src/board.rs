3c8edc5ffc49f0c26c0f162019bae5281e6449ee
Øyvind Ege <oyvind.ege@pm.me>
2025-06-02 13:39:58 +0200

    Moved victory logic checks to the Board struct
    For simplicity, and in anticipation of minimax ai logic

--- src/board.rs
+++ src/board.rs
@@ -185,6 +185,58 @@ impl Board {
             Some(diagonal_data)
         }
     }
+
+    /// Return the encoded player symbol if they have won, None if no victor
+    pub fn check_for_victory(&self) -> Option<CellState> {
+        self.has_horizontal_victor()
+            .or_else(|| self.has_vertical_victor())
+            .or_else(|| self.has_diagonal_victor())
+    }
+
+    fn has_horizontal_victor(&self) -> Option<CellState> {
+        for h in self.get_all_rows() {
+            let has_victor = self.has_victor(&h);
+            if has_victor.is_some() {
+                return has_victor;
+            }
+        }
+        None
+    }
+
+    fn has_vertical_victor(&self) -> Option<CellState> {
+        for v in self.get_all_columns() {
+            let has_victor = self.has_victor(&v);
+            if has_victor.is_some() {
+                return has_victor;
+            }
+        }
+
+        None
+    }
+
+    fn has_diagonal_victor(&self) -> Option<CellState> {
+        self.has_victor(&self.get_diagonal(Diagonal::Major))
+            .or_else(|| self.has_victor(&self.get_diagonal(Diagonal::Minor)))
+    }
+
+    fn has_victor(&self, vec: &[CellState]) -> Option<CellState> {
+        let first = vec.first();
+        first?;
+        match first {
+            Some(t) => match t {
+                CellState::Player(p) => {
+                    if vec.iter().all(|&x| x == CellState::Player(*p)) {
+                        Some(*first.unwrap())
+                    } else {
+                        None
+                    }
+                }
+                CellState::AICellValue(_) => None,
+                CellState::Empty => None,
+            },
+            None => None,
+        }
+    }
 }
 
 #[cfg(test)]
